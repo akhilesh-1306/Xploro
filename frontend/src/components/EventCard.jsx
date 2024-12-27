cd14da197b52c79e2a32e58adfdce0ac2c0d0a39
akhilesh-1306 <chaksu19@gmail.com>
2024-12-27 11:59:01 +0530

    added backend url

--- frontend/src/components/EventCard.jsx
+++ frontend/src/components/EventCard.jsx
@@ -75,7 +75,7 @@ const EventCard = ({ event }) => {
                 <div className="max-w-4xl mx-auto my-8 p-6 bg-white shadow-lg rounded-lg flex flex-col sm:flex-row items-center sm:items-start">
                     <div className="w-full sm:w-1/3 flex-shrink-0 overflow-hidden rounded-lg transition-transform duration-300 transform hover:scale-105">
                         <img
-                            src={event.image ? `xploro-backend.vercel.app/${event.image}` : "https://images.unsplash.com/photo-1556125574-d7f27ec36a06?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+                            src={event.image ? `https://xploro-backend.vercel.app/${event.image}` : "https://images.unsplash.com/photo-1556125574-d7f27ec36a06?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
                             alt={event.activityTitle}
                             className="w-full h-full object-cover"
                         />
@@ -110,7 +110,7 @@ const EventCard = ({ event }) => {
                 </Modal.Header>
                 <Modal.Body className="bg-gray-800 text-white">
                     <img
-                        src={event.image ? `xploro-backend.vercel.app/${event.image}` : "https://images.unsplash.com/photo-1556125574-d7f27ec36a06?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+                        src={event.image ? `https://xploro-backend.vercel.app/${event.image}` : "https://images.unsplash.com/photo-1556125574-d7f27ec36a06?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
                         alt={event.activityTitle}
                         className="w-full h-full object-cover rounded-lg mb-4"
                     />
