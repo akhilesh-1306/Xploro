3bba54a8bc893ba8b9200f8b2279bceaf8019b79
akhilesh-1306 <chaksu19@gmail.com>
2024-12-27 12:40:37 +0530

    deployment changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    historyApiFallback: true, // For local development
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
+});
